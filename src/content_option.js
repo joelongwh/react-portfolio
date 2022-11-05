1e03e9cbae9c545184d705b7493946ad16e1edb8
joelongwh <joelong14@gmail.com>
2022-11-05 16:44:57 +0800

    updated portfolio data

--- src/content_option.js
+++ src/content_option.js
@@ -1,32 +1,32 @@
 const portfoliodata = [{
-  img: "https://picsum.photos/400/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://images.unsplash.com/photo-1516321497487-e288fb19713f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cXVpenxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  description: "Quiz web application to put your programming knowledge to the test",
+  link: "https://github.com/joelongwh/quiz-app",
 },
 {
-  img: "https://picsum.photos/400/800/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRpZ2l0YWwlMjB3YWxsZXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  description: "Digital wallet for users to deposit, transfer and withdraw money",
+  link: "https://github.com/joelongwh/digital-wallet",
 },
 {
-  img: "https://picsum.photos/400/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://images.unsplash.com/photo-1580048915913-4f8f5cb481c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y3VycmVuY3l8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  description: "Currency conversion system to process transactions and update user profiles",
+  link: "https://github.com/joelongwh/currency-converter",
 },
 {
-  img: "https://picsum.photos/400/600/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://media.istockphoto.com/illustrations/simple-illustration-of-hangman-game-illustration-id1196954772?k=20&m=1196954772&s=612x612&w=0&h=nzsr9bCwxp9xW3dp-nBJeXE7TVGqnWtdJpbaXvEyl3E=",
+  description: "Command line version of the classic hangman game",
+  link: "https://github.com/joelongwh/hangman-game",
 },
 {
-  img: "https://picsum.photos/400/300/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjeWNsZSUyMGJpbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  description: "Unix recycle bin to safely delete and restore files",
+  link: "https://github.com/joelongwh/unix-recycle-bin",
 },
 {
-  img: "https://picsum.photos/400/700/?grayscale",
-  description: "The wisdom of life consists in the elimination of non-essentials.",
-  link: "#",
+  img: "https://images.unsplash.com/photo-1595247191348-fd605f104467?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZpbHRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  description: "Interactive web image filter",
+  link: "https://github.com/joelongwh/web-image-filter",
 }
 ];
 
